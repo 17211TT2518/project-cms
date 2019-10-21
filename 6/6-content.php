27f612ce02cd6c6e85c6e07c7031b1468dd6c42a
huyentrang <huyentrang2518.tdc.edu@gmail.com>
2019-10-21 17:20:38 +0700

    21/10-cms

---
+++ 6/6-content.php
@@ -0,0 +1,72 @@
+<div class="type-6">
+	<div class="container">
+		<div class="section-title">
+			<b></b>
+			<span>lastest new</span>
+			<b></b>
+		</div>
+		<div class="swiper-container">
+			<div class="swiper-wrapper">
+				<div class="swiper-slide"><img class="img-responsive" src="./images/1.png" alt="responsive">
+					<div class="square">
+						<span class="number">19</span>
+						<span>Nov</span>
+					</div>
+					<!-- <div class="text">
+						<h5>Welcome to Flatsome</h5>
+						<p>Welcome to WordPress. This is your first post. Edit or delete it, then start blogging! [...] </p>
+					</div> -->
+				</div>
+
+
+				<div class="swiper-slide"><img class="img-responsive" src="./images/2.png" alt="responsive">
+					<div class="square">
+						<span class="number">19</span>
+						<span>Nov</span>
+					</div>
+				</div>
+				<div class="swiper-slide"><img class="img-responsive" src="./images/3.png" alt="responsive">
+					<div class="square">
+						<span class="number">19</span>
+						<span>Nov</span>
+					</div>
+				</div>
+				<div class="swiper-slide"><img class="img-responsive" src="./images/4.png" alt="responsive">
+					<div class="square">
+						<span class="number">19</span>
+						<span>Nov</span>
+					</div>
+				</div>
+				<div class="swiper-slide"><img class="img-responsive" src="./images/5.png" alt="responsive">
+					<div class="square">
+						<span class="number">19</span>
+						<span>Nov</span>
+					</div>
+				</div>
+				<div class="swiper-slide"><img class="img-responsive" src="./images/6.png" alt="responsive">
+					<div class="square">
+						<span class="number">19</span>
+						<span>Nov</span>
+					</div>
+				</div>
+				<div class="swiper-slide"><img class="img-responsive" src="./images/7.png" alt="responsive">
+					<div class="square">
+						<span class="number">19</span>
+						<span>Nov</span>
+					</div>
+				</div>
+				<div class="swiper-slide"><img class="img-responsive" src="./images/8.png" alt="responsive">
+					<div class="square">
+						<span class="number">19</span>
+						<span>Nov</span>
+					</div>
+				</div>
+			</div>
+			<!-- Add Pagination -->
+			<div class="swiper-pagination"></div>
+			<!-- Add Arrows -->
+			<div class="swiper-button-next"></div>
+			<div class="swiper-button-prev"></div>
+		</div>
+	</div>
+</div>
