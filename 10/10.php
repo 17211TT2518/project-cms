4b77b49c4c347c4e7a99269551f274b3c0ecb160
Hoang Van Huy <huynoone@gmail.com>
2019-10-23 14:50:08 +0700

    commit

--- 10/10.php
+++ 10/10.php
@@ -21,13 +21,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="<?php echo $url_path ?>/css/bootstrap-3.3.7.min.css" rel="stylesheet" type="text/css"/>
-        <link href="<?php echo $url_path ?>/css/1.css" rel="stylesheet" type="text/css"/>
-        <link rel="stylesheet" type="text/css" href="<?php echo $url_path ?>/css/fontawesome.min.css">
+        <link href="<?php echo $url_path ?>/css/10.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" type="text/css" href="css/fontawesome.min.css">
         <script src="<?php echo $url_path ?>/js/jquery-3.1.1.min.js"></script>
-        <script src="<?php echo $url_path ?>/js/1.js"></script>
-        <script src="<?php echo $url_path ?>/js/bootstrap-3.3.7.min.js"></script>
+        <script src="<?php echo $url_path ?>/js/10.js"></script>
     </head>
     <body>
-        <?php include '1-content.php'; ?>
+        <?php include '10-content.php'; ?>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     </body>
 </html>
