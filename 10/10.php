8fb4a08f6cd0cc4a61b7ec40cdac02a3bec7ba45
Hoang Van Huy <huynoone@gmail.com>
2019-10-25 01:12:29 +0700

    Finish

--- 10/10.php
+++ 10/10.php
@@ -17,7 +17,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>1</title>
+        <title>10</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="<?php echo $url_path ?>/css/bootstrap-3.3.7.min.css" rel="stylesheet" type="text/css"/>
@@ -25,29 +25,12 @@
         <link rel="stylesheet" type="text/css" href="css/fontawesome.min.css">
         <link rel="stylesheet" href="css/swiper.min.css">
         <script src="<?php echo $url_path ?>/js/jquery-3.1.1.min.js"></script>
+        <script src="<?php echo $url_path ?>/js/swiper.min.js"></script>
+        <script src="<?php echo $url_path ?>/js/bootstrap-3.3.5.min.js"></script>
         <script src="<?php echo $url_path ?>/js/10.js"></script>
     </head>
     <body>
         <?php include '10-content.php'; ?>
-        <script src="js/swiper.min.js"></script>
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-        <script>
-            $("#flipFlop").modal();
-        </script>
-            <script>
-        var swiper = new Swiper('.swiper-container', {
-          slidesPerView: 1,
-          spaceBetween: 30,
-          loop: true,
-          pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-          },
-          navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-          },
-        });
-      </script>
+        <script>$("#flipFlop").modal();</script>
     </body>
 </html>
