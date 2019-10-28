f9f755a086d14453f6428c06bcafce2e057469eb
JosHieu <hieunguyenductt6@gmail.com>
2019-10-28 09:47:18 +0700

    commit

--- 2/2-content.php
+++ 2/2-content.php
@@ -1,8 +1,11 @@
 <div class="type-2">
     <div class="container">
         <div class="row">
-        <div class="col-md-10">sdfsd</div>
-        <div class="col-md-2">dfs</div>
+            <div class="col-md-8 pro-left1">
+            </div>
+            <div class="col-md-4 pro-right1">
+                <img src="images/425781.jpg" alt="pic2" class="img-responsive">
+            </div>
         </div>
     </div>
 </div>
