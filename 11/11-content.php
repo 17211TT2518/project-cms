ee3f4bd386d8509597fd179620a67e47faefeadc
huyentrang <huyentrang2518.tdc.edu@gmail.com>
2019-10-29 18:07:43 +0700

    29/10

--- 11/11-content.php
+++ 11/11-content.php
@@ -9,18 +9,24 @@ $url_path = str_replace('\\', '/', $url_path);
 ?>
 <div class="type-11">
 	<div class="container">
-		<div class="bg">
-			<div class="content">
-				<h1>OUR BLOG</h1>
-				<p>Change This header to anything by using the UX Builder. You can also remove it. It's only visible on the blog homepage.</p>
+		<div class="img">
+			<div class="row text-center">
+				<div class="col-md-3"></div>
+				<div class="col-md-6">
+					<div class="content">
+						<h1>OUR BLOG</h1>
+						<p>Change This header to anything by using the UX Builder. You can also remove it. It's only visible on the blog homepage.</p>
+					</div>
+					<div class="all-icon">
+						<a href="#" data-tooltip="tooltip" data-placement="top" title="facebook"><i id="icon" class="fa fa-facebook" aria-hidden="true"></i></a>
+						<a href="#"><i id="icon" class="fa fa-twitter" aria-hidden="true"></i></a>
+						<a href="#"><i id="icon" class="fa fa-envelope-o" aria-hidden="true"></i></a>
+						<a href="#"><i id="icon" class="fa fa-pinterest-p" aria-hidden="true"></i></a>
+						<a href="#"><i id="icon" class="fa fa-instagram" aria-hidden="true"></i></a>
+					</div>
+				</div>
+				<div class="col-md-3"></div>
 			</div>
-			<div class="all-icon">
-                    <a href="#"><i id="icon" class="fa fa-facebook" aria-hidden="true"></i></a>
-                    <a href="#"><i id="icon" class="fa fa-twitter" aria-hidden="true"></i></a>
-                    <a href="#"><i id="icon" class="fa fa-envelope-o" aria-hidden="true"></i></a>
-                    <a href="#"><i id="icon" class="fa fa-pinterest-p" aria-hidden="true"></i></a>
-                    <a href="#"><i id="icon" class="fa fa-instagram" aria-hidden="true"></i></a>
-                </div>
 		</div>
 	</div>
 </div>
--- 11/11.php
+++ 11/11.php
@@ -22,9 +22,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="<?php echo $url_path ?>/css/bootstrap-3.3.7.min.css" rel="stylesheet" type="text/css"/>
         <link href="<?php echo $url_path ?>/css/11.css" rel="stylesheet" type="text/css"/>
-        <link rel="stylesheet" type="text/css" href="<?php echo $url_path ?>/css/swiper.min.css">
         <link href="<?php echo $url_path ?>/css/font-awesome-4.6.3.min.css" rel="stylesheet">
+
+        <script src="<?php echo $url_path ?>/js/bootstrap-3.3.7.min.js"></script>
         <script src="<?php echo $url_path ?>/js/jquery-3.1.1.min.js"></script>
+        <script src="<?php echo $url_path ?>/js/11.js"></script>
         
     </head>
     <body>
