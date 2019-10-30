e377ce8bb16b93c56b83f0e12cbdcc51cb78c020
huyentrang <huyentrang2518.tdc.edu@gmail.com>
2019-10-30 14:32:10 +0700

    30/10

--- 11/11-content.php
+++ 11/11-content.php
@@ -9,7 +9,6 @@ $url_path = str_replace('\\', '/', $url_path);
 ?>
 <div class="type-11">
 	<div class="container">
-		<div class="img">
 			<div class="row text-center">
 				<div class="col-md-3"></div>
 				<div class="col-md-6">
@@ -18,7 +17,7 @@ $url_path = str_replace('\\', '/', $url_path);
 						<p>Change This header to anything by using the UX Builder. You can also remove it. It's only visible on the blog homepage.</p>
 					</div>
 					<div class="all-icon">
-						<a href="#" data-tooltip="tooltip" data-placement="top" title="facebook"><i id="icon" class="fa fa-facebook" aria-hidden="true"></i></a>
+						<a href="#"><i id="icon" class="fa fa-facebook" aria-hidden="true"></i></a>
 						<a href="#"><i id="icon" class="fa fa-twitter" aria-hidden="true"></i></a>
 						<a href="#"><i id="icon" class="fa fa-envelope-o" aria-hidden="true"></i></a>
 						<a href="#"><i id="icon" class="fa fa-pinterest-p" aria-hidden="true"></i></a>
@@ -27,6 +26,5 @@ $url_path = str_replace('\\', '/', $url_path);
 				</div>
 				<div class="col-md-3"></div>
 			</div>
-		</div>
 	</div>
 </div>
