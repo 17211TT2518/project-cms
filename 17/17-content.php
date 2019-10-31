a0d25be8cd379ea7d6c36568c2cf98a76acbb4a7
tdc-students <cuongtk@tdc.edu.vn>
2019-10-31 11:56:50 +0700

    add module 17

---
+++ 17/17-content.php
@@ -0,0 +1,92 @@
+<div class="type-17">
+   <div class="container">
+    <br><br>
+    <div class="tabs">
+        <ul class="nav nav-tabs">
+            <li><a data-toggle="tab" href="#home">Description</a></li>
+            <li  class="active"><a data-toggle="tab" href="#menu1">Additional information</a></li>
+            <li><a data-toggle="tab" href="#menu2">Reviews (4)</a></li>
+        </ul>
+
+         <div class="tab-content">
+            <div id="home" class="tab-pane fade">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum iaculis massa nec velit commodo lobortis. Quisque diam lacus, tincidunt vitae eros porta, sagittis rhoncus est. Quisque sed justo a erat lobortis gravida. Suspendisse nibh neque, hendrerit vel nisi at, ultrices adipiscing justo. Nunc ullamcorper molestie felis at pharetra.</p>
+              <p>Osaka Entry Tee NOK 399, Superdry - NELLY.COM</p>
+              <p>Marfa authentic High Life veniam Carles nostrud, pickled meggings assumenda fingerstache keffiyeh Pinterest.</p>
+            </div>
+            <div id="menu1" class="tab-pane fade">
+                <div class="row size-pr">
+                    <div class="col-md-6">
+                        <p class="title-size">SIZE</p>
+                    </div>
+                    <div class="col-md-6">
+                        <p>S, M, L</p>
+                    </div>
+                </div>
+            </div>
+            <div id="menu2" class="tab-pane fade  in active">
+                <div class="row">
+                    <div class="col-md-7">
+                        <h3>4 reviews for Osaka Entry Tee Superdry 12</h3>
+                        <div class="comments">
+                            <div class="avatar">
+                                <img src="images/avatar.jpg" class="img-responsive" >
+                            </div>
+                            <div class="comment">
+                                <div class="star-rating">
+                                        <span class="fa fa-star-o" data-rating="1"></span>
+                                        <span class="fa fa-star-o" data-rating="2"></span>
+                                        <span class="fa fa-star-o" data-rating="3"></span>
+                                        <span class="fa fa-star-o" data-rating="4"></span>
+                                        <span class="fa fa-star-o" data-rating="5"></span>
+                                        <input type="hidden" name="whatever1" class="rating-value" value="4.56">
+                                </div>
+                                <div class="name"><b>Tommy Vedvik</b> - August 12, 2013</div>
+                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                            </div>
+                        </div>
+                        <div class="comments">
+                            <div class="avatar">
+                                <img src="images/avatar.jpg" class="img-responsive" >
+                            </div>
+                            <div class="comment">
+                                <div class="star-rating">
+                                        <span class="fa fa-star-o" data-rating="1"></span>
+                                        <span class="fa fa-star-o" data-rating="2"></span>
+                                        <span class="fa fa-star-o" data-rating="3"></span>
+                                        <span class="fa fa-star-o" data-rating="4"></span>
+                                        <span class="fa fa-star-o" data-rating="5"></span>
+                                        <input type="hidden" name="whatever1" class="rating-value" value="4.56">
+                                </div>
+                                <div class="name"><b>Tommy Vedvik</b> - August 12, 2013</div>
+                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                            </div>
+                        </div>
+                        <div class="comments">
+                            <div class="avatar">
+                                <img src="images/avatar.jpg" class="img-responsive" >
+                            </div>
+                            <div class="comment">
+                                <div class="star-rating">
+                                        <span class="fa fa-star-o" data-rating="1"></span>
+                                        <span class="fa fa-star-o" data-rating="2"></span>
+                                        <span class="fa fa-star-o" data-rating="3"></span>
+                                        <span class="fa fa-star-o" data-rating="4"></span>
+                                        <span class="fa fa-star-o" data-rating="5"></span>
+                                        <input type="hidden" name="whatever1" class="rating-value" value="4.56">
+                                </div>
+                                <div class="name"><b>Tommy Vedvik</b> - August 12, 2013</div>
+                                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="col-md-5" style="background: red; height: 500px">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
