8137b55bda6cbc2cd02bf9d4b4ef97993fb78150
Quý Tô <quyto181@gmail.com>
2019-11-02 10:21:06 +0700

    21

--- 7/js/7.js
+++ 7/js/7.js
@@ -1,4 +1,18 @@
 var swiper = new Swiper('.swiper-container', {
+    breakpoints: {
+        400: {
+            slidesPerView: 1,
+            spaceBetween: 50,
+        },
+        600: {
+            slidesPerView: 3,
+            spaceBetween: 50,
+        },
+        999: {
+            slidesPerView: 5,
+            spaceBetween: 30,
+        }
+    },
     slidesPerView: 5,
     spaceBetween: 10,
     loop: true,
